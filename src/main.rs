76c3641260b5cdf923002b7dccbd5f5070760431
Toon Spin <toon.spin@gmail.com>
2022-08-16 02:48:29 +0200

    Add progress bar, add CLI args to tweak debug output

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use core::hash::{Hash, Hasher};
+use indicatif::{ProgressBar, ProgressStyle};
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::Read;
@@ -10,6 +11,14 @@ struct Args {
     /// The path to a file with a list of words
     #[clap(short, long, value_parser)]
     input_file: std::path::PathBuf,
+
+    /// Show a progress indicator on standard error
+    #[clap(short, long, action)]
+    progress: bool,
+
+    /// Add extra output to standard error
+    #[clap(short, long, action)]
+    verbose: bool,
 }
 
 #[derive(Debug)]
@@ -68,57 +77,86 @@ fn all_characters_unique(word: &[u8]) -> bool {
     return true;
 }
 
-fn get_disjoint_indices(word_list: &Vec<Word>, sequence_length: usize) -> Vec<Vec<usize>> {
+fn get_disjoint_indices(
+    word_list: &Vec<Word>,
+    sequence_length: usize,
+    args: &Args,
+) -> Vec<Vec<usize>> {
+    let bar = if args.verbose || !args.progress {
+        ProgressBar::hidden()
+    } else {
+        ProgressBar::new(word_list.len().try_into().unwrap())
+    };
+    bar.set_style(
+        ProgressStyle::default_bar()
+            .template("{elapsed_precise} {wide_bar} {percent}%")
+            .unwrap(),
+    );
     let mut result = Vec::new();
     for i in 0..word_list.len() {
-        println!("{}/{}", i, word_list.len());
+        bar.inc(1);
+        if args.verbose && args.progress {
+            eprint!("{}/{}\r", i, word_list.len());
+        }
         result.append(&mut get_disjoint_indices_partial(
             &word_list,
             sequence_length,
+            args.verbose,
             vec![],
             vec![i],
-            &(0..word_list.len()).collect()
+            &(0..word_list.len()).collect(),
         ));
     }
+    if args.verbose && args.progress {
+        eprintln!("");
+    }
+    bar.finish();
     result
 }
 
 fn get_disjoint_indices_partial(
     word_list: &Vec<Word>,
     sequence_length: usize,
+    verbose: bool,
     mut partial: Vec<Vec<usize>>,
     mut state: Vec<usize>,
-    valid_indices: &Vec<usize>
+    valid_indices: &Vec<usize>,
 ) -> Vec<Vec<usize>> {
     if state.len() == sequence_length {
-        print!("Found:");
-        for i in state.iter() {
-            print!(" {}", word_list[*i].original_word);
+        if verbose {
+            eprint!("Found:");
+            for i in state.iter() {
+                eprint!(" {}", word_list[*i].original_word);
+            }
+            eprintln!("");
         }
-        println!("");
         partial.push(state);
         return partial;
     }
 
     let last_index = *state.last().expect("state must not be empty");
-    let new_valid_indices: Vec<usize> = valid_indices.iter().filter(|&i| word_list[last_index].is_disjoint_with(&word_list[*i])).cloned().collect();
-
-    // if state.len() < 3 {
-    //     for _i in 0..state.len() {
-    //         print!("  ")
-    //     }
-    //     println!("length of new_valid_indices: {:?}", new_valid_indices.len());
-    // }
+    let new_valid_indices: Vec<usize> = valid_indices
+        .iter()
+        .filter(|&i| word_list[last_index].is_disjoint_with(&word_list[*i]))
+        .cloned()
+        .collect();
 
     for next_index in new_valid_indices.iter().filter(|&i| *i >= last_index) {
         state.push(*next_index);
-        partial = get_disjoint_indices_partial(word_list, sequence_length, partial, state.clone(), &new_valid_indices);
+        partial = get_disjoint_indices_partial(
+            word_list,
+            sequence_length,
+            verbose,
+            partial,
+            state.clone(),
+            &new_valid_indices,
+        );
         state.pop();
     }
     partial
 }
 
-fn get_words<T: Read>(mut input_reader: T) -> std::io::Result<Vec<Word>> {
+fn get_words<T: Read>(mut input_reader: T, args: &Args) -> std::io::Result<Vec<Word>> {
     let mut word_set: HashSet<Word> = HashSet::new();
     let mut word_list = String::new();
     input_reader.read_to_string(&mut word_list)?;
@@ -127,7 +165,20 @@ fn get_words<T: Read>(mut input_reader: T) -> std::io::Result<Vec<Word>> {
         bytes.sort();
         if all_characters_unique(&bytes) {
             let word = Word::new(bytes.clone().try_into().unwrap(), String::from(line));
-            word_set.insert(word);
+            if word_set.contains(&word) {
+                let existing = word_set.get(&word).unwrap();
+                if args.verbose {
+                    eprintln!(
+                        "An anagram of the word {} is already in the list ({}).",
+                        word.original_word, existing.original_word
+                    );
+                }
+            } else {
+                if args.verbose {
+                    eprintln!("Adding the word {} to the list.", word.original_word);
+                }
+                word_set.insert(word);
+            }
         }
     }
     Ok(word_set.into_iter().collect())
@@ -135,8 +186,16 @@ fn get_words<T: Read>(mut input_reader: T) -> std::io::Result<Vec<Word>> {
 
 fn main() -> std::io::Result<()> {
     let args = Args::parse();
-    let input_file = File::open(args.input_file)?;
-    let word_list = get_words(input_file)?;
-    get_disjoint_indices(&word_list, 5);
+    let input_file = File::open(args.input_file.clone())?;
+    let word_list = get_words(input_file, &args)?;
+    for sequence in get_disjoint_indices(&word_list, 5, &args).iter() {
+        for i in 0..5 {
+            if i > 0 {
+                print!("\t");
+            }
+            print!("{}", word_list[sequence[i]].original_word);
+        }
+        println!("");
+    }
     Ok(())
 }
